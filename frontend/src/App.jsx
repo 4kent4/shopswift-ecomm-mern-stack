5fa8079a048324914b61d33b64e7d7cc2502969f
4kent4 <kentfortich.ccs@gmail.com>
2023-08-07 13:43:06 +0800

    frontend Home, Product pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<Outlet />
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.jsx";
+import "./assets/styles/index.css";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen.jsx";
+import ProductScreen from "./screen/ProductScreen.jsx";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route index={true} path="/" element={<HomeScreen />} />
+			<Route path="/product/:id" element={<ProductScreen />} />
+		</Route>
+	)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
